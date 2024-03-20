31413dce6663272262af555bbd0d8f18449d0b09
Anna Denysenko <anna.denysenko@ovoko.com>
2024-03-20 16:52:16 +0200

    Added part of task 6

--- HW_6/lesson_6.js
+++ HW_6/lesson_6.js
@@ -1,3 +1,23 @@
 "use strict";
 
-// Task 1
+// // Task 1
+
+// // #1
+
+//   function capitalize(str) {
+//     if(!str) return str;
+
+//     return str[0].toUpperCase() + str.slice(1).toLowerCase();
+//   }
+
+//   console.log(capitalize("Download Report List"));
+
+
+// #2
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("hello"));
+
+// https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
